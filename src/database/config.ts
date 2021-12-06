769d5878c8c0fa561d287d2cedb98c1b88d68dba
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2021-12-06 10:51:08 -0300

    altera arquivo config.ts

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,17 +1,36 @@
+
+// export const connection = createConnection({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "postgres",
+//     password: "root",
+//     database: "chandra_surya",
+//     logging: true,
+//     entities: [
+//         UserEntity, 
+//         ArticleEntity,
+//         CommentEntity,
+//         ConsultationEntity,
+//     ]
+// });
+
 import { createConnection } from "typeorm";
 import { ArticleEntity } from "../entities/article.entity";
 import { CommentEntity } from "../entities/comment.entity";
 import { ConsultationEntity } from "../entities/consultation.entity";
-import { UserEntity } from '../entities/user.entity';
+import { UserEntity } from "../entities/user.entity";
 
 
 export const connection = createConnection({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "root",
-    database: "chandra_surya",
+    url: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+    //host: "localhost",
+    //port: 5432,
+    //username: "postgres",
+    //password: "root",
+    //database: "chandra_surya",
     logging: true,
     entities: [
         UserEntity, 
@@ -20,3 +39,4 @@ export const connection = createConnection({
         ConsultationEntity,
     ]
 });
+
